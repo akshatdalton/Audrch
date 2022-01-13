8de8dcb17646787029afe48e4fb07a84303a19af
akshatdalton <akshat.dak@students.iiit.ac.in>
2022-01-13 20:30:24 +0530

    client:app: Fix the relative issue for the found matches.

--- client/app.py
+++ client/app.py
@@ -1,9 +1,12 @@
+import os
 import streamlit as st
 from helper import get_matches
 
 st.set_page_config(page_title="Jina Audio Search")
 
 st.title("Audrch")
+
+current_abs_path = os.path.abspath(os.path.dirname(__file__))
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
@@ -16,5 +19,5 @@ if uploaded_file is not None:
         st.write("No matches found :(")
 
     for match in matches:
-        st.audio(f"../{match['uri']}")
-
+        path = os.path.join(current_abs_path, "..", match["uri"])
+        st.audio(path)
