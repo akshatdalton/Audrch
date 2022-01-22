7d029dc36764bf2d8a4416dd9d5dff9cd34db044
akshatdalton <akshat.dak@students.iiit.ac.in>
2022-01-23 00:35:43 +0530

    app: Use dataset from Spotify.

--- app.py
+++ app.py
@@ -2,6 +2,56 @@ from jina import DocumentArray, Flow
 
 from jina.types.document.generators import from_files
 
+import os
+import csv
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+def save_song_from_track(sp, track_id):
+    try:
+        track = sp.track(track_id)
+    except Exception:
+        return
+
+    if track["preview_url"] is None:
+        return
+
+    try:
+        res = requests.get(track["preview_url"])
+    except Exception:
+        return
+
+    if not res.ok or not res.content:
+        return
+
+    with open(f"data/{track_id}.mp3", "wb") as f:
+        f.write(res.content)
+
+
+def generate_data():
+    if len(os.listdir("data")) != 0:
+        print("Data alredy present")
+        return
+
+    print("Populating data")
+
+    auth_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    with open("genres_v2.csv") as f:
+        TOTAL_LIMIT = 10
+        curr_limit = 0
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            save_song_from_track(sp, row["id"])
+            curr_limit += 1
+            if curr_limit > TOTAL_LIMIT:
+                break
+
+    print("Data populated!")
+
 
 def check_query(resp):
     for d in resp.docs:
@@ -11,6 +61,7 @@ def check_query(resp):
 
 
 def main():
+    generate_data()
     docs = DocumentArray(from_files("data/*.mp3"))
 
     f = Flow.load_config("flow.yml")
